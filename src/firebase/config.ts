70d1db49350095e1c69f57969ac2d77fb53f7f33
unknown <ensemblemidipyrenees@free.fr>
2026-02-09 19:40:37 +0100

    Suppr Firebase

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-1298593919-88f95",
-  "appId": "1:347602369147:web:0bf4075287902423d0c0de",
-  "apiKey": "AIzaSyCzkXpWo52k3GwQV2do6Ct4WAZFRBRJgq8",
-  "authDomain": "studio-1298593919-88f95.firebaseapp.com",
+  "projectId": "",
+  "appId": "",
+  "apiKey": "",
+  "authDomain": "",
   "measurementId": "",
-  "messagingSenderId": "347602369147"
+  "messagingSenderId": ""
 };
