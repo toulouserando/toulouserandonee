4f0df142384ebab976a996ddc5e6b5768ffef621
unknown <ensemblemidipyrenees@free.fr>
2026-02-09 23:00:20 +0100

    creat dynamic

--- src/app/(app)/events/create/page.tsx
+++ src/app/(app)/events/create/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+export const dynamic = 'force-dynamic';
+
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
