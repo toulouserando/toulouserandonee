2f828b92eb133d5acfedd7c42692a002b148619e
unknown <ensemblemidipyrenees@free.fr>
2026-02-09 20:17:57 +0100

    modif Firebase Config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "",
-  "appId": "",
-  "apiKey": "",
-  "authDomain": "",
-  "measurementId": "",
-  "messagingSenderId": ""
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "toulouserando-dummy",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:123:web:abc",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "dummy-key",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "dummy-domain.firebaseapp.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "123",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || ""
 };
