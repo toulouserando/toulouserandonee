ff7b4631df54488c0b2560476a67fff9e6059bf9
unknown <ensemblemidipyrenees@free.fr>
2026-02-09 20:31:59 +0100

    fix: Use CLient

--- src/app/(app)/community/slug/page.tsx
+++ src/app/(app)/community/slug/page.tsx
@@ -1,7 +1,7 @@
+"use client";
+
 export const dynamic = 'force-dynamic';
 export const fetchCache = 'force-no-store';
-
-"use client";
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
